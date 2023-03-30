319dd5b965bcfb243f8afa7595fc57c9cbb9b20e
akshaygaikwad6 <akshaygaikwad6@byjus.com>
2023-03-30 12:42:31 +0530

    new test case

--- test/api_automation/get.request.apiPractise.js
+++ test/api_automation/get.request.apiPractise.js
@@ -1,32 +1,32 @@
 import pkg from 'pactum';
 const { spec, request, stash } = pkg;
 
+/****************************JSON.stringify(object) = it converts all object data to json and you can print */
 
-
-describe('Get requests', () => {
-    it('get particular user title ', async () => {
+describe('Get HTTP API requests ', () => {
+    it('Get particular user title ', async () => {
 
         let title = await spec().get('https://jsonplaceholder.typicode.com/posts').returns((ctx) => { return ctx.res.json[3].title });
         console.log('*************************User title is ====' + title)
     })
 
-    it('get all users ', async () => {
+    it('Get all users ', async () => {
 
         let data = await spec().get('https://jsonplaceholder.typicode.com/posts').returns((ctx) => { return ctx.res.body });
         console.log('*************************User title is ====' + JSON.stringify(data))
     })
 
-    it('get all users title and users count  ', async () => {
+    it('Get all users title and users count  ', async () => {
 
         let users = await spec().get('https://jsonplaceholder.typicode.com/posts').returns((ctx) => { return ctx.res.body });
         for (let eleme of users) {
-            console.log("******************this is my  tile*****" + JSON.stringify(eleme.title))
+            console.log("******************this is my  tile*****" + eleme.title)
         }
         console.log("Number of users " + users.length)
 
     })
 
-    it('get all users id and get all sum count of ID ', async () => {
+    it('Get all users id and get all sum count of ID ', async () => {
         let sumID = 0;
         let idCount = 0;
         let user = await spec().get('https://jsonplaceholder.typicode.com/posts').returns((ctx) => { return ctx.res.body });
@@ -38,6 +38,18 @@ describe('Get requests', () => {
         console.log("Count of ID *************************" + idCount)
 
     })
+
+    it('Get all data and its email ID', async () => {
+
+        const resposnse = await spec().get('https://jsonplaceholder.typicode.com/comments?postId=1').returns((ctx) => { return ctx.res.body })
+
+        for (let ele in resposnse) {
+            console.log("*********All Email Id ***********" + resposnse[ele].email)
+        }
+        console.log("********************" + typeof (resposnse))
+        console.log("********************" + JSON.stringify(resposnse))
+
+    });
 
 
 });
